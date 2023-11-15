94b58bc307422166b001729a1eaa0135d48cb733
csnizik <csnizik@tulane.edu>
2023-11-14 18:07:57 -0600

    rm cached m & t

--- web/modules/entity_reference_integrity/modules/entity_reference_integrity_enforce/src/Exception/ProtectedEntityException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\entity_reference_integrity_enforce\Exception;
-
-/**
- * An exception thrown when a protected entity cannot be deleted.
- */
-class ProtectedEntityException extends \Exception {
-}
